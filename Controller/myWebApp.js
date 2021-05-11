a1fe43379abfc5ef26878fcef8e96a93aae75cde
BenBlu <benblumenfeld2001@gmail.com>
2021-05-11 22:19:49 +0300

    fixed the web example

--- Controller/myWebApp.js
+++ Controller/myWebApp.js
@@ -3,18 +3,18 @@ const fileUpload = require('express-fileupload')
 const model = require('../Model/SearchInFile')
 
 const app = express()
-app.use(express.urlencoded)({
-    extended:false
-})
+app.use(express.urlencoded({
+    extended: false
+}))
 app.use(fileUpload())
 app.use(express.static('../View'))
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.sendFile('index.html')
 })
-app.post('/search', (req, res)=>{
-    res.write('searching for ' + req.body.key + ':\n')
+app.post('/search', (req, res) => {
+    res.write('searching for ' + req.body.key +':\n')
     var key = req.body.key
-    if(req.files){
+    if(req.files) {
         var file = req.files.text_file
         var result = model.searchText(key, file.data.toString())
         res.write(result)
