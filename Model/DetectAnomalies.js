14caec7a3ffdfe138c8a28d543663352d4a644e8
omer zundelevich <omer.zundelevich@live.biu.ac.il>
2021-05-21 17:18:12 +0300

    the func returns json file with the anomalies that were detected

--- Model/DetectAnomalies.js
+++ Model/DetectAnomalies.js
@@ -1,6 +1,7 @@
 function detect(normal_file, anomaly_file ,algo){
     var normal = normal_file.data.toString()
     var anomaly = anomaly_file.data.toString()
+    console.log(algo)
     if (algo == "Regression")
     {
 
@@ -14,6 +15,7 @@ function detect(normal_file, anomaly_file ,algo){
         console.log("in index.html the value of options is *not* in algo") //delete
     }                                                                      //delete
     //continue later
+    //the func returns json file with the anomalies that were detected
 }
 
 module.exports.detect = detect
