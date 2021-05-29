0043f475828362fe794e0ed3534cf07ef6b1477a
BenBlu <benblumenfeld2001@gmail.com>
2021-05-29 18:07:17 +0300

    Finished working on the code

--- Controller/myWebApp.js
+++ Controller/myWebApp.js
@@ -2,46 +2,7 @@ const express = require('express')
 const fileUpload = require('express-fileupload')
 const model = require('../Model/DetectAnomalies')
 
-// usage of server
-/*
-const WebSocketServer=require('websocket').server
-const WebSocketClient=require('websocket').client
-const WebSocketFrame=require('websocket').frame
-const WebSocketRouter=require('websocket').router
-const W3CWebSocket=require('websocket').w3websocket
-*/
-
 const app = express()
-// usage of server
-/*const client = new WebSocketClient()
-
-client.on('connectFailed', function(error){
-    console.log('Connect Error: ' + error.toString())
-})
-client.on('connect', function(connection){
-    console.log('WebSocket Client Connected')
-    connection.on('error', function(error){
-        console.log('Connect Error: ' + error.toString())
-    })
-    connection.on('close', function(){
-        console.log('echo-protocol Connection Closed')
-    })
-    connection.on('message', function(message){
-        if(message.type === 'utf8'){
-            console.log("Received: '" + message.utf8Data + "'")
-        }
-    })
-
-    function sendNumber(){
-        if(connection.connected){
-            var number = Math.round(Math.random()*0xFFFFFF)
-            connection.sendUTF(number.toString())
-            setTimeout(sendNumber, 1000)
-        }
-    }
-    sendNumber()
-})
-client.connect('ws://localhost:8080/','echo-protocol')*/
 
 app.use(express.urlencoded({
     extended: false
@@ -53,17 +14,12 @@ app.get('/', (req, res) => {
 })
 
 app.post('/detect', async(req, res) => {
-    // res.write('The anomalies which were detected in the flight file info are the following:\n')
-    var chosenAlgo = req.body.algorithms //look into it later
+    var chosenAlgo = req.body.algorithms 
     if(req.files) {
         var normal_file = req.files.normal_csv_file
         var anomaly_file = req.files.anomaly_csv_file
-        //console.log("before\n")
         var result = await model.detect(normal_file, anomaly_file ,chosenAlgo)
-        //console.log("after")
-        //console.log("result=" + JSON.stringify(result.data))
         res.write(JSON.stringify(result.data))
-        //res.write('{"cars":[ {"name":"Ford" , "price":100}, {"name":"BMW", "price":200}, {"name":"Fiat", "price":300} ]}')
         res.end()
     }
 })
--- Model/DetectAnomalies.js
+++ Model/DetectAnomalies.js
@@ -1,25 +1,15 @@
 async function detect(normal_file, anomaly_file ,algo){
     var normal = normal_file.data.toString()
     var anomaly = anomaly_file.data.toString()
-    //var net = require('net');
     const axios = require('axios').default;
-    //var host = 'localhost';
-    //var port = 7550;
-    //var socket = new net.Socket();
-    // socket.on('data', (data) => {
-    //console.log("in\n")
-    //    console.log(`${data}`);
-    //    socket.destroy();
-    // });
     var send = "start\n"
-    var received
     send += normal
     send += "done\n"
     send += anomaly
     send += "done\n"
     if (algo == "Regression")
     {
-        send += "r\n" //simple
+        send += "r\n" 
     }
     else if (algo == "Hybrid")
     {
@@ -30,46 +20,12 @@ async function detect(normal_file, anomaly_file ,algo){
         console.log("in index.html the value of options is *not* in algo") 
     }
     
-    
     var url = "http://localhost:7550/"
     return await axios.post(url, send).then((response) => {
         return response
       }, (error) => {
         console.log(error.toString());
-      });
-
-
-
-    // return await socket.connect(port, host, () => {
-    //     socket.write(send);
-    //     console.log("write")
-    //     return socket.read()
-    //     // received = socket.read()
-    //     // console.log("rec=" + received)
-    // });
-
-    // socket.setTimeout(1000*30)
-    // socket.on('data', function(data) {
-    //     console.log('Received: ' + data);
-    //     received = data
-    //     socket.destroy(); 
-    // });
-
-    // socket.on('timeout', function(timedOutSocket) {
-    //     timedOutSocket.write('socket timed out!');
-    //     timedOutSocket.end();
-    //   });
-    
-    // socket.on('close', function() {
-    //     console.log('Connection closed');
-    // });
-
-    // return received
-    //console.log(received)
-    //continue later
-    //the func returns json file with the anomalies that were detected
-    //var text = {"First":"Hello", "Second":"World"}
-    //return received
+    });
 }
 
 module.exports.detect = detect
